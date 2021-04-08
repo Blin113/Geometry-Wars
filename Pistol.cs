0e83fabac9420ad19dfe60179ae2b588cf62bd98
Blin113 <54438172+Blin113@users.noreply.github.com>
2021-04-08 17:17:19 +0200

    Add files via upload

---
+++ Pistol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Template
+{
+    class Pistol : IShoot
+    {
+        private float reloadTime = 0.5f;
+        List<Bullet> bullets;
+
+        public Pistol(List<Bullet> bullets)
+        {
+            this.bullets = bullets;
+        }
+
+        public float ReloadTime
+        {
+            get { return reloadTime; }
+        }
+
+        void IShoot.Shoot(Vector2 playerPos, float angle, Vector2 speed, Point size, Vector2 mousePos, DamageOrigin damageOrigin)
+        {
+            bullets.Add(new Bullet(Assets.BulletTexture, playerPos, speed, angle, size, mousePos, damageOrigin));
+        }
+    }
+}
---
+++ Shotgun.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Shotgun : IShoot
+    {
+        private float reloadTime = 1;
+        List<Bullet> bullets;
+
+        public Shotgun(List<Bullet> bullets)
+        {
+            this.bullets = bullets;
+        }
+
+        public float ReloadTime
+        {
+            get { return reloadTime; }
+        }
+
+        void IShoot.Shoot(Vector2 playerPos, float angle, Vector2 speed, Point size, Vector2 mousePos, DamageOrigin damageOrigin)
+        {
+            bullets.Add(new Bullet(Assets.BulletTexture, playerPos, speed, angle, size, mousePos, damageOrigin));
+        }
+    }
+}
